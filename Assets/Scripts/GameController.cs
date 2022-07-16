100ea87488b743110cd8d88c063849082d94cd11
Kyle Woodger <kylewoodger@gmail.com>
2022-07-16 13:43:30 +0100

    made the gamecontroller object persistent and made it store the players inventory

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,32 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+
+    public static GameController instance;
 
     public int currentLevel;
     public GamePhase gamePhase;
     public int villageHealth;
     public int aliveEnemies;
+    public int noOfStandardDice; 
+    public int noOfPrecisionDice;
+    public int noOfChoiceDice;
     public bool canThrowDice;
+
+
+    public void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     // Start is called before the first frame update
     public void Start()
     {
         canThrowDice = false;
         villageHealth = 100;
+        noOfStandardDice = 3;
+        noOfPrecisionDice = 1;
+        noOfChoiceDice = 1;
         gamePhase = GamePhase.MENU;
     }
 
     // Update is called once per frame
     public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextLevel();
+        }
     }
 
     public void NextLevel() {
         currentLevel++;
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
     public void StartGame() {
